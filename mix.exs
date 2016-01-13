771f0bb2f36d31b777760195fe28f09822434f21
Damir Gaynetdinov <damir.gaynetdinov@gmail.com>
2016-01-13 23:08:25 +0100

    Phoenix 1.1 and Elixir 1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExMoney.Mixfile do
   def project do
     [app: :ex_money,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -31,19 +31,19 @@ defmodule ExMoney.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.0.3"},
+      {:phoenix, "~> 1.1"},
       {:phoenix_ecto, "~> 2.0"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.1"},
+      {:phoenix_html, "~> 2.3"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:cowboy, "~> 1.0"},
       {:httpoison, "~> 0.8.0"},
       {:guardian, "0.9.0"},
-      {:comeonin, "~> 1.6"},
+      {:comeonin, "~> 2.0"},
       {:ex_machina, "~> 0.6"},
       {:faker, "~> 0.5", only: :test},
       {:logger_file_backend, "0.0.5"},
-      {:timex, "0.19.5"}
+      {:timex, "1.0.0"}
     ]
   end
 
--- web/web.ex
+++ web/web.ex
@@ -18,8 +18,9 @@ defmodule ExMoney.Web do
 
   def model do
     quote do
-      use Ecto.Model
+      use Ecto.Schema
 
+      import Ecto
       import Ecto.Changeset
       import Ecto.Query, only: [from: 1, from: 2]
     end
@@ -30,6 +31,7 @@ defmodule ExMoney.Web do
       use Phoenix.Controller
 
       alias ExMoney.Repo
+      import Ecto
       import Ecto.Model
       import Ecto.Query, only: [from: 1, from: 2]
 
@@ -62,6 +64,7 @@ defmodule ExMoney.Web do
       use Phoenix.Channel
 
       alias ExMoney.Repo
+      import Ecto
       import Ecto.Model
       import Ecto.Query, only: [from: 1, from: 2]
     end
