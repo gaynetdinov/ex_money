fcc49b5d44ab91edd1f3e9862e3a984583ccaf0f
Damir Gaynetdinov <damir.gaynetdinov@gmail.com>
2016-02-27 19:11:54 +0100

    Fix Guardian unauthenticated callback

--- lib/ex_money/guardian/unauthenticated.ex
+++ lib/ex_money/guardian/unauthenticated.ex
@@ -1,6 +1,8 @@
 defmodule ExMoney.Guardian.Unauthenticated do
   alias ExMoney.{Repo, User}
 
+  import Ecto.Query
+
   def unauthenticated(conn, _params) do
     case Repo.one(from u in User, select: count(u.id)) do
       0 -> Phoenix.Controller.redirect(conn, to: "/setup/new")
