4f5e0dd9f32b7ce9b3e874f72c92c751edeca192
Damir Gaynetdinov <damir.gaynetdinov@fyber.com>
2016-03-22 18:04:33 +0100

    Consider the case when rule's target does not exist anymore

--- web/views/settings/rule_view.ex
+++ web/views/settings/rule_view.ex
@@ -2,10 +2,16 @@ defmodule ExMoney.Settings.RuleView do
   use ExMoney.Web, :view
 
   def target_name("assign_category", target_id, categories, _accounts) do
-    "Category: #{categories[target_id].name}"
+    case categories[target_id] do
+      nil -> "Stale target, remove this rule"
+      category -> "Category: #{category.name}"
+    end
   end
 
   def target_name("withdraw_to_cash", target_id, _categories, accounts) do
-    "Account: #{accounts[target_id].name}"
+    case accounts[target_id] do
+      nil -> "Stale target, remove this rule"
+      account -> "Account: #{account.name}"
+    end
   end
 end
