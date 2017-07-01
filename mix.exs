2cb73b8fba086cfbc8528b4813bde6e35e0ee2da
Damir Gaynetdinov <damir.gaynetdinov@gmail.com>
2017-07-01 14:07:20 +0200

    Update phoenix_html

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule ExMoney.Mixfile do
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.6"},
+      {:phoenix_html, "~> 2.9"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:cowboy, "~> 1.0"},
       {:httpoison, "~> 0.12"},
